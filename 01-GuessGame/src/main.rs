dcf6409cef04567f5d3eee57bf45e21ce4272549
SoraSpades <davidandrino2002@gmail.com>
2023-06-04 22:08:30 +0200

    01-GuessGame

--- 01-GuessGame/src/main.rs
+++ 01-GuessGame/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        print!("Enter guess: ");
+        io::stdout().flush().expect("Error Flushing stdout");
+        io::stdin().read_line(&mut guess).expect("Failed to read");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Bigger"),
+            Ordering::Greater => println!("Smaller"),
+            Ordering::Equal => { 
+                println!("Win");
+                break;
+            }
+        }
+    }
 }
